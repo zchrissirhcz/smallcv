181eb47271a695eba3ef43a4b9829ad0df700ac8
Zhuo Zhang <imzhuo@foxmail.com>
2020-12-09 19:53:34 +0800

    fix memory leak with shared_ptr
    Hoping it can bring real RAII

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+
+#ifdef VLD // for memory leak detection
+#include <vld.h>
+#endif
+
 #include <stdio.h>
 #include <string>
+#include <memory>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_PSD
@@ -30,7 +36,7 @@ namespace sv {
     class Mat {
         // public member functions
         public:
-            Mat(Shape3d shape, uchar* data);
+            Mat(Shape3d shape);
             ~Mat();
             size_t get_height() const { return height; }
             size_t get_width() const { return width; }
@@ -38,32 +44,29 @@ namespace sv {
 
         // public data members
         public:
-            uchar* data;
+            std::shared_ptr<uchar> data;
 
         // private data members
         private:
-            bool data_raii; // if data created in ctor, data_raii should be true
-
             size_t height;
             size_t width;
             size_t channels;
     };
 
-    Mat::Mat(Shape3d shape, uchar* _data)
+    Mat::Mat(Shape3d shape)
     {
         height = shape.height;
         width = shape.width;
         channels = shape.channels;
-        data = _data;
-        data_raii = false;
+        size_t size = height * width * channels * sizeof(unsigned char);
+        data.reset(new uchar[size], [](uchar* p) {
+            delete[] p;
+        });
     }
 
     Mat::~Mat()
     {
-        if (data_raii) {
-            free(data);
-            data = NULL;
-        }
+        data.reset();
     }
 
 }
@@ -80,14 +83,19 @@ sv::Mat imread(const char* image_path)
     int height;
     int width;
     int channels;
-    unsigned char* data = stbi_load(image_path, &width, &height, &channels, 0);
-    assert(data!=NULL);
-
+    unsigned char* raw_data = stbi_load(image_path, &width, &height, &channels, 0);
+    assert(raw_data!=NULL);
+    assert(channels==3); // TODO: support gray image and detect 4-channel RGBA image
     Shape3d shape;
     shape.height = static_cast<size_t>(height);
     shape.width = static_cast<size_t>(width);
     shape.channels = static_cast<size_t>(channels);
-    sv::Mat image(shape, data);
+    sv::Mat image(shape);
+    uchar* image_data = image.data.get();
+    size_t size = height * width * channels * sizeof(unsigned char);
+    memcpy(image_data, raw_data, size);
+    free(raw_data);
+
     return image;
 }
 
@@ -105,21 +113,19 @@ void imwrite(const char* filename, const sv::Mat& image)
     const char* ext = filename + strlen(filename) - 4;
     int width = static_cast<int>(image.get_width());
     int height = static_cast<int>(image.get_height());
+    uchar* data = image.data.get();
     assert(image.get_channels()==3); // TODO: support gray image
-    int save_status = 0;
+    assert(data!=NULL);
     if (0==strcmp(ext, ".jpg")) {
         int quality = 100;
-        save_status = stbi_write_jpg(filename, width, height, 3, image.data, quality);
-        assert(save_status!=0);
+        stbi_write_jpg(filename, width, height, 3, data, quality);
     }
     else if (0==strcmp(ext, ".png")) {
         int stride_in_bytes = width * 3; // TODO: Mat may use line alignment
-        save_status = stbi_write_png(filename, width, height, 3, image.data, stride_in_bytes);
-        assert(save_status!=0);
+        stbi_write_png(filename, width, height, 3, data, stride_in_bytes);
     }
     else if (0==strcmp(ext, ".bmp")) {
-        save_status = stbi_write_bmp(filename, width, height, 3, image.data);
-        assert(save_status!=0);
+        stbi_write_bmp(filename, width, height, 3, data);
     }
     else {
         fprintf(stderr, "%s format is not supported yet!\n", ext);
@@ -138,22 +144,29 @@ void rgb_bgr_swap_inplace(sv::Mat& image)
     size_t w = image.get_width();
     size_t c = image.get_channels();
     assert(c==3);
+    uchar* data = image.data.get();
     for (size_t i=0; i<h; i++) {
         for (size_t j=0; j<w; j++) {
             size_t idx = i*w*3 + j*3;
-            unsigned char t = image.data[idx];
-            image.data[idx] = image.data[idx+2];
-            image.data[idx+2] = t;
+            unsigned char t = data[idx];
+            data[idx] = data[idx+2];
+            data[idx+2] = t;
         }
     }
 }
 
 int main() {
-    std::string image_path = "mingren.jpg";
-    sv::Mat image = imread(image_path);
-    rgb_bgr_swap_inplace(image);
-    imwrite("mingren_swap.jpg", image);
-    imwrite("mingren_swap.bmp", image);
+    {
+        std::string image_path = "mingren.jpg";
+        sv::Mat image = imread(image_path);
+        rgb_bgr_swap_inplace(image);
+        imwrite("mingren_swap.jpg", image);
+        imwrite("mingren_swap.bmp", image);
+    }
+
+#ifdef VLD
+    VLDReportLeaks();
+#endif
 
     return 0;
 }
