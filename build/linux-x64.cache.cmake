8c8fc17b2380bc7dd6f61632727bc4aa168a5ad9
Zhuo Zhang <imzhuo@foxmail.com>
2020-12-12 16:17:51 +0800

    fix ubuntu20.04 imshow crash (use glfw-3.3-stable)
    https://github.com/glfw/glfw/issues/1633

--- build/linux-x64.cache.cmake
+++ build/linux-x64.cache.cmake
@@ -1,2 +1,2 @@
 set(ncnn_DIR "/home/zz/soft/ncnn/20201208/lib/cmake/ncnn" CACHE PATH "Directory that contain ncnnConfig.cmake")
-set(glfw3_DIR "/home/zz/soft/glfw/3.3.2/lib/cmake/glfw3" CACHE PATH "Directory that contains glfw3Config.cmake")
+set(glfw3_DIR "/home/zz/soft/glfw/3.3-stable/lib/cmake/glfw3" CACHE PATH "Directory that contains glfw3Config.cmake")
